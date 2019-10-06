402696a37e2ec021d361f932e04baf86dc343be9
Emin Keçik <Eminkecik8@gmail.com>
2019-10-06 16:56:46 +0300

    Add project files.

---
+++ --06_10_2019-- HesapMakinesi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __06_10_2019___HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
